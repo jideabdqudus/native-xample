c16c1a8e91d4d40795523cfac3047cf8b30b9f04
jideabdqudus <jideabdqudus@gmail.com>
2020-07-04 14:11:42 +0100

    Added commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,8 +3,10 @@ import { Text, StyleSheet, View } from "react-native";
 import ComponentsScreen from "./ComponentsScreen";
 
 const HomeScreen = () => {
+  const onLaunch = "Wagwan!"
   return (
-    <View>
+    <View styles={styles.view}>
+      <Text style={styles.onLaunch}>{onLaunch}</Text>
       <Text style={styles.text}>Hi, Abdul-Qudus</Text>
       <ComponentsScreen />
     </View>
@@ -15,6 +17,14 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
   },
+  onLaunch:{
+    color: "#fbca02",
+    marginLeft: 40,
+    paddingTop: 10
+  },
+  view:{
+    marginLeft: 60,
+  }
 });
 
 export default HomeScreen;
