dbb0b554b90b267195e2bfc2eb3354366b17795c
jideabdqudus <jideabdqudus@gmail.com>
2020-07-04 14:39:54 +0100

    Added commit

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,18 +1,56 @@
-import React from 'react'
-import {Text, View, StyleSheet} from "react-native"
+import React from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
-    return (
-        <View>
-            <Text style={styles.text}>List Screen</Text>
-        </View>
-    )
-}
+  const friends = [
+    {
+      name: "Friend 1",
+      id: "1",
+    },
+    {
+      name: "Friend 2",
+      id: "2",
+    },
+    {
+      name: "Friend 3",
+      id: "3",
+    },
+    {
+      name: "Friend 4",
+      id: "4",
+    },
+    {
+      name: "Friend 5",
+      id: "5",
+    },
+    {
+      name: "Friend 6",
+      id: "6",
+    },
+    {
+      name: "Friend 7",
+      id: "7",
+    },
+  ];
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(friend) => friend.name}
+        data={friends}
+        renderItem={(friend) => {
+          return <Text>{friend.name}</Text>;
+        }}
+      />
+      <Text style={styles.text}>List Screen</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    text:{
-        fontSize:20
-    }
-})
+  text: {
+    fontSize: 20,
+  },
+});
 
-export default ListScreen
+export default ListScreen;
