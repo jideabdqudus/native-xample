16a7744953d54309c4a9e5ce9888463a439e7667
jideabdqudus <jideabdqudus@gmail.com>
2020-07-05 15:24:23 +0100

    Added commit

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
 
 const ImageDetail = () => {
     return (
         <View>
-            <Text>Image Detail</Text>        
+            <Text>Image Detail</Text> 
+            <Image source={require('../../assets/beach.jpg')}/>       
         </View>
     )
 }
