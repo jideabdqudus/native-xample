468764d41e2fdf571392323328727ea518949ac0
jideabdqudus <jideabdqudus@gmail.com>
2020-07-05 21:36:27 +0100

    Added commit

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import {View, Text, Button, StyleSheet} from 'react-native'
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const SquareScreen = () => {
-    return (
-        <View>
-            <Text>Square Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Square Screen</Text>
+      <View>
+        <Text>Red</Text>
+        <Button title="More Red" onPress={} />
+        <Button title="Less Red" onPress={} />
+      </View>
+      <View>
+        <Text>Green</Text>
+        <Button title="More Green" onPress={} />
+        <Button title="Less Green" onPress={} />
+      </View>
+      <View>
+        <Text>Blue</Text>
+        <Button title="More Blue" onPress={} />
+        <Button title="Less Blue" onPress={} />
+      </View>
+      <View>
+        <Text>Red</Text>
+        <View style={{ height: 100, width: 100, backgroundColor: item }} />
+      </View>
+    </View>
+  );
+};
 
-const styles= StyleSheet.create({})
+const randomRgb = () => {
+    
+};
 
-export default SquareScreen
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
