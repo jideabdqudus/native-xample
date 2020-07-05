5ef5d63771e9004ec7ae47ccd6f07a58ede3fc0f
jideabdqudus <jideabdqudus@gmail.com>
2020-07-05 15:32:20 +0100

    Added commit

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {View, Text, StyleSheet, Image} from 'react-native'
 
-const ImageDetail = () => {
+const ImageDetail = ({title}) => {
     return (
         <View>
-            <Text>Image Detail</Text> 
+            <Text>Image Detail - {title}</Text> 
             <Image source={require('../../assets/beach.jpg')}/>       
         </View>
     )
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -6,7 +6,7 @@ const ImageScreen = () => {
     return (
         <View>
             <Text>Image Screen</Text>
-            <ImageDetail/>
+            <ImageDetail title="Beach"/>
         </View>
     )
 }
